646a2d91d0a590978b491d278221f05f871a81ba
David Sangaran <davidsangaran@outlook.sg>
2023-04-12 18:08:14 +0800

    added index.js and minor tweaks to styles.css and index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+console.log("script running!")
+var titleHeaderHeight = document.querySelector(".title-header").offsetHeight;
+var aboutElem = document.querySelector(".title-header").offsetHeight;
+
+console.log(titleHeaderElem)
+
