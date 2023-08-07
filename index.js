01dadc56286d94f085cc4f88cadc10558e6bb948
David Sangaran <davidsangaran@outlook.sg>
2023-08-07 12:23:41 +0800

    Major changes, changed html SVG to be less repeating by using use tag. Unified SVG styles in CSS, and lastly added random SVG generator for stars in top div

--- index.js
+++ index.js
@@ -1,8 +1,59 @@
+var star = document.getElementById("svg-star")
+var starCount = 300;
+
+var starArr = cloneSVG(star, starCount);
+
+spreadSVG(starArr);
 
 
-console.log("script running!")
-var titleHeaderHeight = document.querySelector(".title-header").offsetHeight;
-var aboutElem = document.querySelector(".title-header").offsetHeight;
 
-console.log(titleHeaderElem)
+
+
+
+
+
+function cloneSVG(svg, count){
+
+    var svgArr = [];
+
+    for( i=0 ; i<count ; i++ ){
+        
+        svgArr.push(svg.cloneNode(true));
+
+    }
+    
+    return svgArr
+};
+
+function spreadSVG(svgArr){
+
+    let colorArr=['#afc9ff','#c7d8ff','#fff4f3','#ffe5cf','#ffd9b2','#ffc78e','#ffa651',]
+    
+    let screenWidth = window.innerWidth;
+    let screenHeight = window.innerHeight;
+
+    let svgWidth = star.clientWidth;
+
+    screenWidth += svgWidth;
+    screenHeight += svgWidth;
+
+    for( i=0 ; i<svgArr.length ; i++ ){
+
+        let randX = Math.random()*screenWidth;
+        let randY = Math.random()*screenHeight;
+        let randwidth = Math.random()*(svgWidth*0.75);
+        let randAngle = Math.random()*360;
+        let randcolor = Math.floor(Math.random()*colorArr.length);
+
+        svgArr[i].style.left = `${randX}px`
+        svgArr[i].style.top = `${randY}px`
+        svgArr[i].style.transform = `rotate(${randAngle}deg)`
+        svgArr[i].style.width = `${randwidth}px`
+        svgArr[i].style.height = `${randwidth}px`
+        svgArr[i].style.fill = `${colorArr[randcolor]}`
+        document.getElementById("star-div").appendChild(svgArr[i]);
+
+    }
+
+}
 
