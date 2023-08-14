d9fef21fa099309ed59b90853cf79091e9db8283
David Sangaran <davidsangaran@outlook.sg>
2023-08-14 17:59:09 +0800

    added second div, added animation of person SVG sliding in. Updated Javascript to include Intersection Observer. We can now animate on scroll! Also added deviation for stars distribution

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var star = document.getElementById("svg-star")
 var star8 = document.getElementById("svg-8star")
-console.log(star8.querySelector("path"))
 var starCount = 120;
 var lineArr = document.getElementsByClassName("name-constellation-svg")
 var starArr = cloneSVG(star,star8, starCount);
@@ -13,6 +12,18 @@ drawLine(lineArr);
     scrollToY(1000, 1000); // Example: Scroll to y=500 pixels over 1000 milliseconds
   }); */
 
+const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach(entry => {
+            console.log(entry.target.id)
+            document.querySelector("#person").classList.toggle("person-in", (entry.isIntersecting && entry.target.id == "second-div"))
+
+        });    
+
+    },{threshold: 0.6});
+
+const elementList = document.querySelectorAll(".scrollElement") 
+elementList.forEach((elem) => observer.observe(elem));
 
 
 
@@ -29,7 +40,6 @@ function cloneSVG(svgType1,svgType2, count){
             currSvg = svgType1.cloneNode(true);
             currSvg.removeAttribute('id');
             currSvg.style.visibility = "visible"; 
-            console.log(currSvg)
             svgArr.push(currSvg.cloneNode(true));
             
         }else{
@@ -61,7 +71,14 @@ function spreadSVG(svgArr){
     for( i=0 ; i<svgArr.length ; i++ ){
 
         let randX = Math.random()*screenWidth;
-        let randY = Math.random()*screenHeight;
+        /* let randY = Math.random()*screenHeight; */ /* Previous PosY value */
+        let randY = () => {
+            const u1 = Math.random();
+            const u2 = Math.random();
+            let z0 = Math.sqrt(-2 * Math.log(u1)) * Math.cos(2 * Math.PI * u2); /* This line is a standard deviation for the stars to appear elss frequently the lower down the page*/
+            z0 < 0 ? z0 *= -1: z0 *= 1; /* Normalizing negative numbers to positive */
+            return Math.floor(z0*screenHeight);
+        };
         let randwidth = Math.random()* (svgWidth*0.9 - svgWidth*0.1) + svgWidth*0.1;
         let randAngle = Math.random()*360;
 
@@ -71,7 +88,7 @@ function spreadSVG(svgArr){
         let randTime = Math.random() * (3 - 1) + 1;
         
         svgArr[i].style.left = `${randX}px`
-        svgArr[i].style.top = `${randY}px`
+        svgArr[i].style.top = `${randY()}px`
         //svgArr[i].style.transform = `rotate(${randAngle}deg)`
         svgArr[i].style.width = `${randwidth}px`
         svgArr[i].style.height = `${randwidth}px`
